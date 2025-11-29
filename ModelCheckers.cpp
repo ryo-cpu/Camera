57a520e45e3d684ba41363ae8f657b27725491e5
tajima1009 <namivvv1215@gmail.com>
2025-11-29 10:57:23 +0900

    当たり判定新しくした

--- ModelCheckers.cpp
+++ ModelCheckers.cpp
@@ -18,5 +18,80 @@ void ModelCheckers::ShowFrameName(int Model)
     {
         DrawFormatString(0, i * 10, GetColor(255, 255, 255), "Name                  %s", MV1GetFrameName(Model, i));
     }
+  
+}
+
+VECTOR ModelCheckers::VMax(VECTOR V1, VECTOR V2)
+{
+    return VSize(V1) > VSize(V2) ? V1 : V2;
+}
+
+VECTOR ModelCheckers::VMax(VECTOR V1, VECTOR V2, VECTOR V3)
+{
+    VECTOR MAX = VMax(V1, V2);
+    MAX = VMax(MAX, V3);
+    return MAX;
+}
+
+VECTOR ModelCheckers::VMin(VECTOR V1, VECTOR V2)
+{
+    return VSize(V1) < VSize(V2) ? V1 : V2;;
+}
+
+VECTOR ModelCheckers::VMin(VECTOR V1, VECTOR V2, VECTOR V3)
+{
+    VECTOR MIN = VMin(V1, V2);
+    MIN = VMin(MIN, V3);
+    return MIN;
+}
+
+VECTOR ModelCheckers::VProject(VECTOR CheckPoint, VECTOR StartGround, VECTOR EndGround)
+{
+    VECTOR Ground = VSub(EndGround, StartGround);
+    ////CheckPintは引数と別れていると考える　変更しても引数側を改変しない
+    CheckPoint = VSub(CheckPoint, StartGround);
+    VECTOR  Proj = VScale(Ground, (VDot(CheckPoint, Ground) / VDot(Ground, Ground)));
+    Proj = VAdd(Proj, StartGround);
+    return Proj;
+}
+
+bool ModelCheckers::IsTriangle_Joint_Sphere(VECTOR T1, VECTOR T2, VECTOR T3, VECTOR SphereP, float R)
+{
+    ////階層分けによって計算を減らす
+    bool isJoint = false;
+
+    VECTOR PT1 = VSub(T1, SphereP);
+    VECTOR PT2 = VSub(T2, SphereP);
+    VECTOR PT3 = VSub(T3, SphereP);
+    VECTOR Min = VMin(PT1, PT2, PT3);
+    if (VSize(Min) >= R)
+    {
+     
+        VECTOR T1T2 = VSub(SphereP,VProject(SphereP, T1, T2));
+        VECTOR T1T3 = VSub(SphereP, VProject(SphereP, T1, T3));
+        VECTOR T2T3 = VSub(SphereP, VProject(SphereP, T2, T3));
+
+     
+        Min = VMin(T1T2,T1T3,T2T3);
+        if (VSize(Min) >= R)
+        {
+            ///法線ベクトル
+            VECTOR N = VCross(VSub(T1, T2), VSub(T1, T3));
+            float D = -(T1.x * N.x + T1.y * N.y + T1.z * N.z);
+
+            float Size =fabs(SphereP.x * N.x + SphereP.y * N.y + SphereP.z * N.z + D) / VSize(N);
+
+            return R >= Size;
+
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        return true;
+    }
 }
 
--- ModelCheckers.h
+++ ModelCheckers.h
@@ -1,9 +1,26 @@
 #pragma once
 #include"DxLib.h"
+#include<cmath>
  class ModelCheckers
 {
 public:
    void ShowTextureName(int Model);
    void ShowFrameName(int Model);
+   VECTOR VMax(VECTOR V1, VECTOR V2);
+   /// @brief 三角形に使うように3つ
+   /// @param V1 
+   /// @param V2 
+   /// @param V3 
+   /// @return 
+   VECTOR VMax(VECTOR V1, VECTOR V2,VECTOR V3);
+   VECTOR VMin(VECTOR V1, VECTOR V2);
+   VECTOR VMin(VECTOR V1, VECTOR V2, VECTOR V3);
+   /// @brief 正射影ベクトルの終点を返す　垂線を下した先を示す
+   /// @param CheckPoint 
+   /// @param StartGround 
+   /// @param EndGround 
+   /// @return 
+   VECTOR VProject(VECTOR CheckPoint,VECTOR StartGround,VECTOR EndGround);
+   bool IsTriangle_Joint_Sphere(VECTOR T1, VECTOR T2, VECTOR T3, VECTOR SphereP, float R);
 };
 
