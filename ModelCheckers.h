a7c7676add3833eb8bfb6df5a8ca611f8baaf0bf
Ibisasdas <116286963+Ibisasdas@users.noreply.github.com>
2026-01-17 16:20:18 +0900

    コメント

--- ModelCheckers.h
+++ ModelCheckers.h
@@ -23,8 +23,29 @@ public:
    /// @param EndGround 
    /// @return 
    VECTOR VProject(VECTOR CheckPoint,VECTOR StartGround,VECTOR EndGround);
+   /// @brief 平行か調べる
+   /// @param A1 
+   /// @param A2 
+   /// @param B1 
+   /// @param B2 
+   /// @return 
    bool IsParallel(VECTOR A1, VECTOR A2, VECTOR B1, VECTOR B2);
+   /// @brief 三角形と球の検知
+   /// @param T1 
+   /// @param T2 
+   /// @param T3 
+   /// @param SphereP 
+   /// @param R 
+   /// @return 
    bool IsTriangle_Joint_Sphere(VECTOR T1, VECTOR T2, VECTOR T3, VECTOR SphereP, float R);
+   /// @brief 三角形同士の検知
+   /// @param TA1 
+   /// @param TA2 
+   /// @param TA3 
+   /// @param TB1 
+   /// @param TB2 
+   /// @param TB3 
+   /// @return 
    bool IsTriangle_Joint_Triangle(VECTOR TA1, VECTOR TA2, VECTOR TA3, VECTOR TB1, VECTOR TB2, VECTOR TB3);
    void TProject(const VECTOR Triangle[3], const VECTOR& Axis, float& OutMin, float& OutMax);
    bool IsModel_Joint_Model(const int& M1, const int& M2, float M1_R);
